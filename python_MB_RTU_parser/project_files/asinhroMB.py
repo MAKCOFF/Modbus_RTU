d006a3cd6805b0a7414d5ec7fbfa18b29b6f145d
Макс <bern-007@mail.ru>
2021-12-22 02:10:29 +0900

    upd

--- python_MB_RTU_parser/project_files/asinhroMB.py
+++ python_MB_RTU_parser/project_files/asinhroMB.py
@@ -3,18 +3,22 @@ import traceback
 from pymodbus.client.sync import ModbusSerialClient as pyRtu
 from _print import _print
 
+# portNbr = "COM4"
+# portName = 'com4'
+# baud_rate = 9600  # 153600
+# parity_E = "E"
+# timeoutSp = 0.1
 
-portNbr = "COM4"
-portName = 'com4'
-baud_rate = 9600  # 153600
-parity_E = "E"
-timeoutSp = 0.1
+setting_RTU = {
+    "method": 'rtu',
+    "port": 'COM4',
+    "baudrate": 9600,
+    "timeout": 0.1,
+}
+arguments_for_write = ([16], 8, 0)
+arguments_for_read = ([16], 16, 0)
 
-argum_to_write = ([16], 8, 0)
-argum_to_read = ([16], 16, 0)
-global dv
-
-pymc = pyRtu(method='rtu', port=portNbr, baudrate=baud_rate, timeout=timeoutSp, parity=parity_E, stopbits=1)
+pymc = pyRtu(setting_RTU)
 
 
 async def read_holding_regs_while(slaves_arr, regs_sp, begin_sp):
@@ -40,7 +44,6 @@ async def read_holding_regs_while(slaves_arr, regs_sp, begin_sp):
 
 
 async def write_holding_reg(slaves_arr, value, address):
-    global dv
     for k in range(1):
 
         await asyncio.sleep(1)
@@ -51,7 +54,7 @@ async def write_holding_reg(slaves_arr, value, address):
 
             try:
                 data = pymc.write_register(address, value, unit=slaveId)
-                dv = data
+                # dv = data
                 # print("\r", data, end="")
             except AttributeError:
                 err_cnt += 1
@@ -62,10 +65,13 @@ async def write_holding_reg(slaves_arr, value, address):
 
 
 async def main():
-    task1 = asyncio.create_task(read_holding_regs_while(*argum_to_read))
-    task2 = asyncio.create_task(write_holding_reg(*argum_to_write))
+    task1 = asyncio.create_task(read_holding_regs_while(*arguments_for_read))
+    task2 = asyncio.create_task(write_holding_reg(*arguments_for_write))
 
     await asyncio.gather(task1, task2)
+
+
+# class MBAsyncScraper:
 
 
 if __name__ == '__main__':
