9c04baf3566f6f7ef1396c2324d32dc99fba17b5
Макс <bern-007@mail.ru>
2022-01-07 05:45:03 +0900

    Обновление

--- python_MB_RTU_parser/project_files/MB_App/MB_App.py
+++ python_MB_RTU_parser/project_files/MB_App/MB_App.py
@@ -34,8 +34,8 @@ class MBScraper(client_RTU):
 
     # count_obj_of_class = 0
     slaves_arr = [16]  # default value
-    regs_sp = 1  # default value
-    begin_sp = 0  # default value
+    quantity_registers = 1  # default value
+    number_first_registers = 0  # default value
 
     def __init__(self, slaves_arr, regs_sp, begin_sp, mode_read_registers=1):
 
@@ -43,9 +43,9 @@ class MBScraper(client_RTU):
         # print(f"Created obj of MBScraper : {self.count_obj}")
         self.data_result = []
         self.slaves_arr = slaves_arr
-        self.regs_sp = regs_sp
-        self.begin_sp = begin_sp
-        self.tb = None
+        self.quantity_registers = regs_sp
+        self.number_first_registers = begin_sp
+        self.traceback_error = None
         self.result = []
         self.mode_read_registers = mode_read_registers
         self.obj_func = None
@@ -79,32 +79,32 @@ class MBScraper(client_RTU):
         for slave_id in self.slaves_arr:
             self.slave_id_ = slave_id
             self.data_result.insert(0, self.slave_id_)  # Вставляем в начало списка адрес слэйва int
-            for i in range(self.regs_sp):
+            for i in range(self.quantity_registers):
                 match self.mode_read_registers:
                     case 1:  # HOLDING    ЧТЕНИЕ
-                        self.obj_func = MBScraper.read_holding_regs(self, i + self.begin_sp, slave_id)
+                        self.obj_func = self.read_holding_regs(i + self.number_first_registers, slave_id)
                         if self.obj_func == "None":
                             err_cnt += 1
                         self.data_result.append(self.obj_func)
                     case 2:  # INPUT      ЧТЕНИЕ
-                        self.obj_func = MBScraper.read_input_regs(self, i + self.begin_sp, slave_id)
+                        self.obj_func = self.read_input_regs(i + self.number_first_registers, slave_id)
                         if self.obj_func == "None":
                             err_cnt += 1
                         self.data_result.append(self.obj_func)
                     case 3:  # DISCRETE   ЧТЕНИЕ
-                        self.obj_func = MBScraper.read_discrete_inputs_regs(self, i + self.begin_sp, slave_id)
+                        self.obj_func = self.read_discrete_inputs_regs(i + self.number_first_registers, slave_id)
                         if self.obj_func == "None":
                             err_cnt += 1
                         self.data_result.append(self.obj_func)
                     case 4:  # COIL       ЧТЕНИЕ
-                        self.obj_func = MBScraper.read_coil_regs(self, i + self.begin_sp, slave_id)
+                        self.obj_func = self.read_coil_regs(i + self.number_first_registers, slave_id)
                         if self.obj_func == "None":
                             err_cnt += 1
                         self.data_result.append(self.obj_func)
         fact_reg = len(self.data_result) - 1 - err_cnt  # Отнимаем от длины списка индекс адреса слэйва -1
         MBScraper.printing_to_cons(self, fact_reg, err_cnt)
 
-        self.result = [self.data_result, fact_reg, self.tb, err_cnt]
+        self.result = [self.data_result, fact_reg, self.traceback_error, err_cnt]
         return self.result
 
     def printing_to_cons(self, fact_reg, err_cnt):
@@ -116,26 +116,26 @@ class MBScraper(client_RTU):
                       "Считано c устройства", self.slave_id_, "HOLDING регистров", fact_reg, "\n",
                       self.data_result)
                 if err_cnt > 0:
-                    print("   !pymodbus:\terr_cnt: %s; last tb: %s" % (err_cnt, self.tb))
+                    print("   !pymodbus:\terr_cnt: %s; last tb: %s" % (err_cnt, self.traceback_error))
             case 2:
                 print("Запрошено", len(self.data_result) - 1,
                       "регистров по одному(size 2 BYTE) за каждый запрос \n",
                       "Считано c устройства", self.slave_id_, "INPUT регистров", fact_reg, "\n", self.data_result)
                 if err_cnt > 0:
-                    print("   !pymodbus:\terr_cnt: %s; last tb: %s" % (err_cnt, self.tb))
+                    print("   !pymodbus:\terr_cnt: %s; last tb: %s" % (err_cnt, self.traceback_error))
             case 3:
                 print("Запрошено", len(self.data_result) - 1,
                       "регистров по одному(size 1 BIT) за каждый запрос \n",
                       "Считано c устройства", self.slave_id_, "DISCRETE INPUT регистров", fact_reg, "\n",
                       self.data_result)
                 if err_cnt > 0:
-                    print("   !pymodbus:\terrCnt: %s; last tb: %s" % (err_cnt, self.tb))
+                    print("   !pymodbus:\terrCnt: %s; last tb: %s" % (err_cnt, self.traceback_error))
             case 4:
                 print("Запрошено", len(self.data_result) - 1,
                       "регистров по одному(size 1 BIT) за каждый запрос \n",
                       "Считано c устройства", self.slave_id_, "COIL регистров", fact_reg, "\n", self.data_result)
                 if err_cnt > 0:
-                    print("   !pymodbus:\terrCnt: %s; last tb: %s" % (err_cnt, self.tb))
+                    print("   !pymodbus:\terrCnt: %s; last tb: %s" % (err_cnt, self.traceback_error))
 
     def read_holding_regs(self, i, slave_id):
         data = self.client.read_holding_registers(i, 1, unit=slave_id)
@@ -144,7 +144,7 @@ class MBScraper(client_RTU):
             meta_data = data.registers
             return "".join(map(str, meta_data))  # Преобразуем из списка в строку
         else:
-            self.tb = traceback.format_exc()
+            self.traceback_error = traceback.format_exc()
             return "None"
 
     def read_input_regs(self, i, slave_id):
@@ -154,7 +154,7 @@ class MBScraper(client_RTU):
             meta_data = data.registers
             return "".join(map(str, meta_data))
         else:
-            self.tb = traceback.format_exc()
+            self.traceback_error = traceback.format_exc()
             return "None"
 
     def read_discrete_inputs_regs(self, i, slave_id):
@@ -164,7 +164,7 @@ class MBScraper(client_RTU):
             data_read.append(data.bits[0])
             return "".join(map(str, data_read))
         else:
-            self.tb = traceback.format_exc()
+            self.traceback_error = traceback.format_exc()
             return "None"
 
     def read_coil_regs(self, i, slave_id):
@@ -174,7 +174,7 @@ class MBScraper(client_RTU):
             data_read.append(data.bits[0])
             return "".join(map(str, data_read))
         else:
-            self.tb = traceback.format_exc()
+            self.traceback_error = traceback.format_exc()
             return "None"
 
 
