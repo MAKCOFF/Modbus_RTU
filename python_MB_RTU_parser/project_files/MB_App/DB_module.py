79e89b99d398d126b958ca1fe71b8a866334fe9e
MAKCOFF <96383761+MAKCOFF@users.noreply.github.com>
2022-01-24 12:35:49 +0900

    Обновление

--- python_MB_RTU_parser/project_files/MB_App/DB_module.py
+++ python_MB_RTU_parser/project_files/MB_App/DB_module.py
@@ -16,18 +16,13 @@ def get_values_from_db():
     return values
 
 
-def insert_value_to_db():
-    with sqlite3.connect('database.db') as db:
-        cursor = db.cursor()
-        query = """ INSERT INTO start (value) VALUES (1) """
-        cursor.execute(query)
-        db.commit()
-
-
 def change_values_in_db(start):
     with sqlite3.connect('database.db') as db:
         cursor = db.cursor()
-        query = f'UPDATE start SET value = {start}'
+        try:
+            query = f'UPDATE start SET value = {start}'
+        except sqlite3.DatabaseError as err:
+            print("Ошибка:", err)
         cursor.execute(query)
         db.commit()
 
@@ -43,6 +38,15 @@ def get_stop_from_db():
         for i in cursor:
             value = i[0]
     return value
+
+
+def insert_value_to_db():
+    with sqlite3.connect('database.db') as db:
+        cursor = db.cursor()
+        query = """ INSERT INTO start (value) VALUES (1) """
+        cursor.execute(query)
+        db.commit()
+
 
 # def create_table_in_db():
 #     with sqlite3.connect('database.db') as db:
